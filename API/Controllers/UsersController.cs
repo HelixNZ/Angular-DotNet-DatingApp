9e3e9881ef15e88cea1ddd3c0873f527a32ddd08
Jake F <helixnz@live.com>
2021-12-09 17:04:37 -0700

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -31,6 +32,22 @@ namespace API.Controllers
 		public async Task<ActionResult<MemberDto>> GetUser(string username)
 		{
 			return await _userRepository.GetMemberAsync(username);
+		}
+
+		[HttpPut]
+		public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+		{
+			//Section 10; 119. Persisting
+			var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			var user = await _userRepository.GetUserByUsernameAsync(username);
+
+			_mapper.Map(memberUpdateDto, user); //automapper
+
+			_userRepository.Update(user); //flag for update
+
+			if(await _userRepository.SaveAllAsync()) return NoContent();
+
+			return BadRequest("Failed to update user");
 		}
 	}
 }
